dffc24688f2e49148ed1a45a25465563292ed813
JaySon-Huang <tshent@qq.com>
2026-03-30 20:39:17 +0800

    test: isolate client package database
    Derive a package-specific test database from external DAT9_MYSQL_DSN so pkg/client does not race other packages over shared schema state during full-suite runs.

--- pkg/client/testmain_test.go
+++ pkg/client/testmain_test.go
@@ -17,6 +17,13 @@ func TestMain(m *testing.M) {
 		log.Fatalf("setup mysql test instance: %v", err)
 	}
 	testDSN = inst.DSN
+	if os.Getenv("DAT9_MYSQL_DSN") != "" {
+		isolatedDSN, err := testmysql.PrepareIsolatedDatabase(inst.DSN, "client")
+		if err != nil {
+			log.Fatalf("prepare isolated client test database: %v", err)
+		}
+		testDSN = isolatedDSN
+	}
 
 	code := m.Run()
 	if err := inst.Close(context.Background()); err != nil {
