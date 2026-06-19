bde68e02e2b39d3c779a3a72431d3e82d7aa145e
mornyx <mornyx.z@gmail.com>
2026-06-19 11:57:23 +0000

    fix: update drain imports to drive9 module path

--- pkg/fuse/mount_control_unix.go
+++ pkg/fuse/mount_control_unix.go
@@ -15,9 +15,9 @@ import (
 
 	"go.uber.org/zap"
 
-	"github.com/mem9-ai/dat9/pkg/logger"
-	"github.com/mem9-ai/dat9/pkg/mountcontrol"
-	"github.com/mem9-ai/dat9/pkg/mountstate"
+	"github.com/mem9-ai/drive9/pkg/logger"
+	"github.com/mem9-ai/drive9/pkg/mountcontrol"
+	"github.com/mem9-ai/drive9/pkg/mountstate"
 )
 
 type mountControlServer struct {
