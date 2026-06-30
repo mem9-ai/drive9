8412573852ff13cf7dd8f5e1b4d934c16649ccc2
srstack <srstack0203@gmail.com>
2026-07-01 00:20:00 +0800

    Fix DB probe test lint

--- pkg/metrics/db_test.go
+++ pkg/metrics/db_test.go
@@ -24,7 +24,7 @@ func (c fakeConnector) Connect(context.Context) (driver.Conn, error) {
 	if !c.healthy.Load() {
 		return nil, errors.New("fake db unreachable")
 	}
-	return fakeConn{healthy: c.healthy, ping: c.ping}, nil
+	return fakeConn(c), nil
 }
 
 func (c fakeConnector) Driver() driver.Driver { return fakeDriver{} }
